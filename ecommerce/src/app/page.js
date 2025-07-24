df94faa3ef1bedec3df5c7676d1721ada27bb572
Jackychan0201 <150186719+Jackychan0201@users.noreply.github.com>
2025-07-24 12:16:31 +0200

    chadcn button

--- ecommerce/src/app/page.js
+++ ecommerce/src/app/page.js
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div>
+    <main className="flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold text-center mt-20">HELLO WORLD</h1>
-    </div>
+      <Button className="mt-30" variant="default" size="lg">Request</Button>
+    </main>
   );
 }
