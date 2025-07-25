e81551dc73ec16b2fd9bb65a9e296efbfa59ba87
Jackychan0201 <150186719+Jackychan0201@users.noreply.github.com>
2025-07-25 21:21:06 +0200

    image fetch

--- src/components/ui/Product.jsx
+++ src/components/ui/Product.jsx
@@ -1,6 +1,6 @@
 export const Product = ({ blok }) => {
   return (
-    <div className="flex flex-col items-center justify-center border-2 border-black-200">
+    <div className="flex flex-col items-center justify-center  border-2 border-black-200">
         <img src={blok.image} width="200" height="200"></img>
         <h1>{blok.title}</h1>
         <p>Price: {blok.price}</p>
