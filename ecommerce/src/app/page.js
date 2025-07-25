a086e766b03ebb9ac99e34329812538f3b1cfd45
Jackychan0201 <150186719+Jackychan0201@users.noreply.github.com>
2025-07-25 11:02:39 +0200

    header

--- ecommerce/src/app/page.js
+++ ecommerce/src/app/page.js
@@ -1,5 +1,6 @@
 import { getStoryblokApi } from '@/app/storyblok';
 import { StoryblokStory } from '@storyblok/react/rsc';
+import { Header } from '@/components/ui/Header';
 
 export async function fetchData() {
 	const storyblokApi = getStoryblokApi();
@@ -15,7 +16,7 @@ export default async function Home() {
 
   return (
     <main className="flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold text-center mt-10">E-Commerce</h1>
+      <Header></Header>
       <ul className="mt-20 grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-10">
           {data.stories.map((story) => (
             <li key={story.id}>
---
+++ ecommerce/src/components/ui/Header.jsx
@@ -0,0 +1,54 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+import Link from "next/link"
+
+export const Header = () => {
+    return (
+        <div className="w-full flex flex-row justify-center items-center">
+            <NavigationMenu className="absolute left-0">
+                <NavigationMenuItem className="flex xs:hidden">
+                    <NavigationMenuTrigger>Navigation</NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        <NavigationMenuLink asChild>
+                                <Link href="/" className="text-xs">Main Page</Link>
+                            </NavigationMenuLink>
+                        <NavigationMenuLink asChild>
+                                <Link href="/page" className="text-xs">FAQ</Link>
+                        </NavigationMenuLink>
+                        <NavigationMenuLink asChild>
+                                <Link href="/page" className="text-xs">About</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+                <NavigationMenuList className="hidden xs:flex">
+                    <NavigationMenuItem>
+                        <NavigationMenuLink asChild>
+                            <Link href="/" className="text-xs">Main Page</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink asChild>
+                            <Link href="/page" className="text-xs">FAQ</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink asChild>
+                            <Link href="/page" className="text-xs">About</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+            <div>
+                <p className="text-sm font-bold text-center sm:text-base md:text-lg lg:text-2xl">E-Commerce</p>
+            </div>
+        </div>
+    )
+}
