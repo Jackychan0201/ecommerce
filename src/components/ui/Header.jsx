5a4b8245f7c1599fddfdadd1c9d126c137149da1
Jackychan0201 <150186719+Jackychan0201@users.noreply.github.com>
2025-07-25 18:26:34 +0200

    navigationn v8

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,63 +1,59 @@
-import { 
+import {
   NavigationMenu,
   NavigationMenuContent,
-  NavigationMenuIndicator,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
-  NavigationMenuViewport,
 } from "@/components/ui/navigation-menu"
 import Link from "next/link"
 
 export const Header = () => {
-    return (
-        <div className="w-full flex flex-row justify-center items-center">
-            <NavigationMenu className="absolute left-0">
-                <NavigationMenuItem className="block xs:hidden">
-                    <NavigationMenuTrigger>Navigation</NavigationMenuTrigger>
-                    <NavigationMenuContent>
-                        <NavigationMenuList>
-                            <NavigationMenuItem>
-                                <NavigationMenuLink asChild>
-                                    <Link href="/" className="text-xs">Home</Link>
-                                </NavigationMenuLink>
-                            </NavigationMenuItem>   
-                            <NavigationMenuItem>
-                                <NavigationMenuLink asChild>
-                                    <Link href="/page" className="text-xs">FAQ</Link>
-                                </NavigationMenuLink>
-                            </NavigationMenuItem>
-                            <NavigationMenuItem>
-                                <NavigationMenuLink asChild>
-                                    <Link href="/page" className="text-xs">About</Link>
-                                </NavigationMenuLink>
-                            </NavigationMenuItem>
-                        </NavigationMenuList>
-                    </NavigationMenuContent>
-                </NavigationMenuItem>
-    
-                <NavigationMenuList className="hidden xs:flex">
-                    <NavigationMenuItem>
-                        <NavigationMenuLink asChild>
-                            <Link href="/" className="text-xs">Home</Link>
-                        </NavigationMenuLink>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <NavigationMenuLink asChild>
-                            <Link href="/page" className="text-xs">FAQ</Link>
-                        </NavigationMenuLink>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <NavigationMenuLink asChild>
-                            <Link href="/page" className="text-xs">About</Link>
-                        </NavigationMenuLink>
-                    </NavigationMenuItem>
-                </NavigationMenuList>
-            </NavigationMenu>
-            <div>
-                <p className="text-sm font-bold text-center sm:text-base md:text-lg lg:text-2xl">E-Commerce</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="w-full flex justify-center items-center relative h-10">
+      <NavigationMenu className="absolute left-2">
+        <NavigationMenuItem className="block sm:hidden">
+          <NavigationMenuTrigger className="text-xs">Menu</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="flex flex-col">
+              <li>
+                <Link href="/" className="text-xs">Home</Link>
+              </li>
+              <li>
+                <Link href="/page" className="text-xs">FAQ</Link>
+              </li>
+              <li>
+                <Link href="/page" className="text-xs">About</Link>
+              </li>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+
+        <NavigationMenuList className="hidden sm:flex">
+          <NavigationMenuItem>
+            <NavigationMenuLink asChild>
+              <Link href="/" className="text-xs">Home</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink asChild>
+              <Link href="/page" className="text-xs">FAQ</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink asChild>
+              <Link href="/page" className="text-xs">About</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+
+      {/* Centered title */}
+      <div className="mx-auto">
+        <p className="text-sm font-bold text-center sm:text-base md:text-lg lg:text-2xl">
+          E-Commerce
+        </p>
+      </div>
+    </div>
+  )
 }
