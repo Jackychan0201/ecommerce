5d28064e7c2631a7632204e0b21e3cf0bf2e4b85
Jackychan0201 <150186719+Jackychan0201@users.noreply.github.com>
2025-07-26 21:44:16 +0200

    storyblok.js fix

--- src/app/storyblok.js
+++ src/app/storyblok.js
@@ -1,10 +1,12 @@
-import { storyblokInit, apiPlugin } from "@storyblok/react";
+import { storyblokInit, apiPlugin, getStoryblokApi as getSbApi } from "@storyblok/react";
 import { ProductCard } from "@/components/ui/ProductCard";
 
-export const getStoryblokApi = storyblokInit({
+storyblokInit({
   accessToken: process.env.STORYBLOK_API,
   use: [apiPlugin],
   components: {
     product: ProductCard
-  }
+  },
 });
+
+export const getStoryblokApi = () => getSbApi();
